1ada2df6c2608e5e0276427215c3d487362f8843
Nicole Martin <n.martinpdx@gmail.com>
2021-04-06 21:38:28 -0700

    Post test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,33 +3,37 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Plant = require('../lib/models/Plant');
+const { response } = require('../lib/app');
 
 describe('Plant-Store Routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('GET ROUTE for a list of plants', async () => {
+  it('POST ROUTE to add a plant to the list', async () => {
+    const response = await request(app)
+      .post('/api/v1/plants')
+      .send({ plant_name: 'hosta', size: 'medium', color: 'green and white' });
+
+    expect(response.body).toEqual({
+      id: '1',
+      plantName: 'hosta',
+      size: 'medium',
+      color: 'green and white',
+    });
+  });
+
+  it.skip('GET ROUTE for a list of plants', async () => {
     const plants = await Promise.all([
       Plant.insert({
-        plantName: 'hosta',
+        plant_name: 'hosta',
         size: 'medium',
         color: 'green and white',
-      }),
-      Plant.insert({
-        plantName: 'snake',
-        size: 'small',
-        color: 'bright green',
-      }),
-      Plant.insert({
-        plantName: 'rose',
-        size: 'medium',
-        color: 'red',
       }),
     ]);
 
     return request(app)
-      .get('api/v1/plants')
+      .get('/api/v1/plants')
       .then((res) => {
         expect(res.body).toEqual(expect.arrayContaining(plants));
       });
