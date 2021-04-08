d0f811695031b4b85d0fe54f7f1c3194c657cea9
Nicole Martin <n.martinpdx@gmail.com>
2021-04-08 11:39:48 -0700

    Add Pots routes and passing tests. Both Plants and Pots routes working.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,10 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Plant = require('../lib/models/Plant');
+const Pot = require('../lib/models/Pot');
 const { response } = require('../lib/app');
 
-describe('Plant-Store Routes', () => {
+describe('PLANTS Routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -16,6 +17,15 @@ describe('Plant-Store Routes', () => {
       plantName: 'hosta',
       size: 'medium',
       color: 'green and white',
+    });
+  });
+
+  beforeEach(async () => {
+    pot = await Pot.insert({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
     });
   });
 
@@ -79,6 +89,84 @@ describe('Plant-Store Routes', () => {
       plantName: 'hosta',
       size: 'medium',
       color: 'green and white',
+    });
+  });
+});
+
+describe('POTS Routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  beforeEach(async () => {
+    pot = await Pot.insert({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
+    });
+  });
+
+  it('POST ROUTE to add a pot to the list', async () => {
+    const response = await request(app)
+      .post('/api/v1/pots')
+      .send({ size: 'large', color: 'black', kind: 'matte' });
+
+    expect(response.body).toEqual({
+      id: '2',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
+    });
+  });
+
+  it('GET ROUTE for pots', async () => {
+    const res = await request(app).get('/api/v1/pots');
+
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        size: 'large',
+        color: 'black',
+        kind: 'matte',
+      },
+    ]);
+  });
+
+  it('GET BY ID pot by its id', async () => {
+    const res = await request(app).get('/api/v1/pots/1');
+
+    expect(res.body).toEqual({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
+    });
+  });
+
+  it('PUT/UPDATE pot by its :id', async () => {
+    const res = await request(app).put('/api/v1/pots/1').send({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
+    });
+
+    expect(res.body).toEqual({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
+    });
+  });
+
+  it('DELETE pot by its :id', async () => {
+    const res = await request(app).delete('/api/v1/pots/1');
+    expect(res.body).toEqual({
+      id: '1',
+      size: 'large',
+      color: 'black',
+      kind: 'matte',
     });
   });
 });
